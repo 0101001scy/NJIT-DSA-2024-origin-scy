fe80f2299aee251f6c3581b92ec97bee3afb4a2b
0101001scy <s910377361@163.com>
2024-03-08 19:35:14 +0800

    1

---
+++ src/main/java/oy/tol/tra/Grades.java
@@ -0,0 +1,18 @@
+package oy.tol.tra;  
+  
+public class Grades {  
+   private Integer[] grades = null;  
+  
+   public Grades(Integer[] grades) {  
+      this.grades = grades;     
+   }  
+   public void reverse() {      
+      Algorithms.reverse(grades);  
+   }  
+   public void sort() {   
+      Algorithms.sort(grades);  
+   }  
+   public Integer[] getArray() {  
+      return grades;  
+   }  
+}
